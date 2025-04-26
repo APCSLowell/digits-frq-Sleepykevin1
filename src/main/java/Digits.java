f13c4b734bc5eb9d0b6ab2439095878c2573ae79
Sleepykevin1 <Hunterkevin12398@gmail.com>
2025-04-25 23:00:23 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -3,31 +3,32 @@ import java.util.ArrayList;
 public class Digits
 {
 
-	private ArrayList<Integer> digitList;
+    private ArrayList<Integer> digitList;
 
-	public Digits(int num)
-	{ 
-digitlist = new ArrayList < Integer>();
-if(num == 0)
-digitList.add(0);
-int n = num;
-while(n > 0){
-digitList.add(0,n%10);
-n=n/10;
+    public Digits(int num)
+    { /* to be implemented in part (a) */ 
+        digitList = new ArrayList <Interger>();
+        if(num==0)
+            digitList.add(0);
+        int n = num;
+        while(n>0){
+            digitList.add(0,n%10);
+            n=n/10;
+        }
+    }
 
-}
-	}
+    public boolean isStrictlyIncreasing()
+    { /* to be implemented in part (b) */
+        for(int i = 0;i<digitList.size()-1;i++){
+            if(digitList.get(i+1)<=digitList.get(i))
+                return false;
+        }
+        return true;
 
-	public boolean isStrictlyIncreasing()
-	{ for(int i = 0; i ‹ digitlist.size()-1;i++)
-if(digitList.get(i+1)<=digitList.get(i))
-return false;
-return true;
-
-	}
-	
-	public String toString()
-	{
-		return digitList.toString();
-	}
+    }
+    
+    public String toString()
+    {
+        return digitList.toString();
+    }
 }
